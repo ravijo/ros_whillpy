775b52b9c927abdcf02eea5e1c3a884a237d4def
Ravi Joshi <ravi2008joshi@gmail.com>
2018-10-05 15:50:09 +0900

    added info in shell script

--- scripts/whill.sh
+++ scripts/whill.sh
@@ -6,8 +6,18 @@
 # $ cd ~/ros_ws
 # $ source whill.sh
 
-# make sure we don't have the variable set already
+# make sure we don't have these variables set already
+unset your_ip
+unset your_hostname
 unset baxter_hostname
+
+# ip address of this machine (raspberrypi board)
+your_ip="172.17.69.112"
+
+# hostname of this machine (raspberrypi board)
+# make sure hostname is resolvable. if not,
+# assign the ip address to hostname as well
+your_hostname="172.17.69.112"
 
 # hostname of the baxter robot
 baxter_hostname="011312P0016.local"
@@ -15,8 +25,12 @@ baxter_hostname="011312P0016.local"
 # first initialize all ROS variables
 source devel/setup.bash
 
+# next modify ROS_IP and ROS_HOSTNAME
+export ROS_IP="${your_ip}"
+export ROS_HOSTNAME="${your_hostname}"
+
 # next modify ROS_MASTER_URI to point out to baxter comptuer
 export ROS_MASTER_URI="http://${baxter_hostname}:11311"
 
 # show ROS_MASTER_URI in shell prompt
-export PS1="[whill - ${ROS_MASTER_URI}] ${PS1}"
+export PS1="[whill] ${PS1}"
