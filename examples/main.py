84d235419b2fe6896ff031d893b6782ceb304dbf
Ravi Joshi <ravi2008joshi@gmail.com>
2018-10-09 19:06:21 +0900

    add service node and client files

---
+++ examples/main.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# controller.py: controller service node
+# Author: Ravi Joshi
+# Date: 2018/10/09
+
+# import modules
+import rospy
+from ros_whillpy.srv import Connect, ConnectRequest, ConnectResponse
+from ros_whillpy.srv import Power, PowerRequest, PowerResponse
+from ros_whillpy.srv import Move, MoveRequest, MoveResponse
+
+
+def main():
+    # make sure that services are available
+    rospy.wait_for_service('connect')
+    rospy.wait_for_service('power')
+    rospy.wait_for_service('move')
+
+    try:
+        connect = rospy.ServiceProxy('connect', Connect, persistent=False)
+        response = connect('/dev/ttyUSB0')
+        print 'response from connect service %s' % response.message
+
+        power = rospy.ServiceProxy('power', Power, persistent=False)
+        response = power(True)
+        print 'response from power service %s' % response.message
+
+        move = rospy.ServiceProxy('move', Move, persistent=False)
+        response = move(20, -80)
+        print 'response from move service %s' % response.message
+    except rospy.ServiceException, e:
+        print 'Service call failed: %s' % e
+
+
+if __name__ == '__main__':
+    main()
---
+++ scripts/controller.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# controller.py: controller service node
+# Author: Ravi Joshi
+# Date: 2018/10/09
+
+# import modules
+import rospy
+import whillpy
+from ros_whillpy.srv import Connect, ConnectRequest, ConnectResponse
+from ros_whillpy.srv import Power, PowerRequest, PowerResponse
+from ros_whillpy.srv import Move, MoveRequest, MoveResponse
+
+
+class Controller:
+    def __init__(self):
+        self.whill = None
+        rospy.init_node('ros_whillpy_service_node')
+
+        # define services
+        connect_service = rospy.Service(
+            'connect', Connect, self.handle_connect)
+        power_service = rospy.Service('power', Power, self.handle_power)
+        move_service = rospy.Service('move', Move, self.handle_move)
+
+        rospy.loginfo('Service ros_whillpy is initialized successfully')
+        rospy.spin()
+
+    def handle_connect(self, req):
+        message = 'Success'
+        try:
+            self.whill = whillpy.connect(port=req.port)
+        except Exception as e:
+            message = str(e)
+            rospy.logerr(message)
+        return ConnectResponse(message)
+
+    def handle_power(self, req):
+        response = PowerResponse()
+        message = 'Success'
+        try:
+            response.success = self.whill.set_power(req.option)
+        except Exception as e:
+            message = str(e)
+            rospy.logerr(message)
+        response.message = message
+        return response
+
+    def handle_move(self, req):
+        response = MoveResponse()
+        message = 'Success'
+        try:
+            response.success = self.whill.move(
+                straight=req.straight, turn=req.turn)
+        except Exception as e:
+            message = str(e)
+            rospy.logerr(message)
+        response.message = message
+        return response
+
+    def __del__(self):
+        ''' cleanup the serial connection object
+        '''
+        if self.whill:
+            self.whill._connection.close()
+
+
+if __name__ == '__main__':
+    Controller()
